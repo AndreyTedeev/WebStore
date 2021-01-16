6c2cab5b9f024511ec27b8654e12f1086fb3f091
Andrey Tedeev <andrey.tedeev@outlook.com>
2021-01-16 12:33:31 +0700

    Add project files.

---
+++ ReadMe.txt
@@ -0,0 +1,5 @@
+﻿Andrey Tedeev <andrey.tedeev@outlook.com>
+
+C# ASP.NET Core. Уровень 1
+
+
---
+++ WebStore/Startup.cs
@@ -0,0 +1,43 @@
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+namespace WebStore
+{
+    public class Startup
+    {
+        private IConfiguration Configuration { get; }
+
+        public Startup(IConfiguration configuration) {
+            Configuration = configuration;
+        }
+
+        public void ConfigureServices(IServiceCollection services) {
+            services.AddControllersWithViews().AddRazorRuntimeCompilation();
+        }
+
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env) {
+            if (env.IsDevelopment()) {
+                app.UseDeveloperExceptionPage();
+            }
+            
+            app.UseStaticFiles();
+            
+            app.UseRouting();
+
+            app.UseEndpoints(endpoints => {
+                
+                endpoints.MapGet("/test", async context => {
+                    await context.Response.WriteAsync(Configuration["TestMessage"]);
+                });
+
+                endpoints.MapControllerRoute(
+                   "default",
+                   "{controller=Home}/{action=Index}/{id?}");
+            });
+        }
+    }
+}

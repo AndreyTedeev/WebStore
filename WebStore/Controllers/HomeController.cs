bd1e930cc175d020272612e79758f2b7e8853b3a
Andrey Tedeev <andrey.tedeev@outlook.com>
2021-01-20 11:27:45 +0700

    add Employees controller and pages

--- WebStore/Controllers/HomeController.cs
+++ WebStore/Controllers/HomeController.cs
@@ -1,48 +1,10 @@
 ﻿using Microsoft.AspNetCore.Mvc;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using WebStore.Models;
 
 namespace WebStore.Controllers {
 
     public class HomeController : Controller {
 
-        private static readonly List<Employee> _employees = new() {
-            new() {
-                Id = 1,
-                LastName = "Иванов",
-                FirstName = "Иван",
-                Patronymic = "Иванович",
-                Birthday = new DateTime(1971, 11, 23),
-                EmploymentStart = new DateTime(2010, 12, 1)
-            },
-            new() {
-                Id = 2,
-                LastName = "Петров",
-                FirstName = "Пётр",
-                Patronymic = "Петрович",
-                Birthday = new DateTime(1981, 9, 3),
-                EmploymentStart = new DateTime(2014, 2, 16)
-            },
-            new() {
-                Id = 3,
-                LastName = "Сидоров",
-                FirstName = "Сидор",
-                Patronymic = "Сидорович",
-                Birthday = new DateTime(1991, 7, 30),
-                EmploymentStart = new DateTime(2016, 9, 26)
-            },
-        };
-
-        public IActionResult Index() =>
-            View();
-
-        public IActionResult Employees() =>
-            View(_employees);
-
-        public IActionResult EmployeeCard(int id) =>
-            View(_employees.Where((e) => e.Id == id).Single());
+        public IActionResult Index() => View();
 
     }
 }

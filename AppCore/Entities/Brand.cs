84561c4508a38962939bbd710891ef811de5f3bf
Andrey Tedeev <andrey.tedeev@outlook.com>
2021-01-27 12:58:03 +0700

    add entities Brand and ProductCategory

---
+++ AppCore/Entities/Brand.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WebStore.Entities.Base;
+using WebStore.Entities.Base.Interfaces;
+
+namespace WebStore.Entities
+{
+    public class Brand : NamedEntity, IOrderedEntity
+    {
+        public int OrderNumber { get; set; }
+    }
+}

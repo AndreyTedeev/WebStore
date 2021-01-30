b5e87b6e7737432add17f749d0670b6d87629dc7
Andrey Tedeev <andrey.tedeev@outlook.com>
2021-01-30 10:54:16 +0700

    add DBContext

---
+++ Infrastructure/Data/WebStoreDB.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using WebStore.Entities;
+
+namespace WebStore.Data
+{
+    public class WebStoreDB : DbContext
+    {
+        public DbSet<Product> Products { get; set; }
+
+        public DbSet<Brand> Brands { get; set; }
+
+        public DbSet<Category> Categories { get; set; }
+
+        public WebStoreDB(DbContextOptions<WebStoreDB> options) : base(options) { }
+
+
+    }
+}
